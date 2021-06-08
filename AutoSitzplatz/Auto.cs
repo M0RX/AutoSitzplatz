4d2a2fec0c9918c5a7650b1ec01805a0a5bd437f
Moritz Lehmann <codeexceptionde@gmail.com>
2021-06-08 22:33:42 +0200

    Projektdateien hinzufügen.

---
+++ AutoSitzplatz/Auto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+public class Auto
+{
+	private int sitzplaetze;
+	private int belegung;
+
+	public Auto(int size)
+	{
+		this.sitzplaetze = size;
+		this.belegung = 0;
+	}
+	public void einsteigen(int anzahl)
+    {
+		if(belegung + anzahl > sitzplaetze)
+        {
+			Console.WriteLine("Ich bin schon voll");
+        } else
+        {
+			belegung += anzahl;
+			sendStatus();
+        }
+    }
+	public void aussteigen(int anzahl)
+    {
+		if(belegung - anzahl < 0)
+        {
+			Console.WriteLine("So viele Menschen sitzten hier doch nicht!");
+        } else
+        {
+			belegung -= anzahl;
+			sendStatus();
+        }
+    }
+	private void sendStatus()
+    {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.Append("Es sitzen {0} Menschen im Auto");
+		Console.WriteLine(stringBuilder.ToString(), belegung);
+    }
+}
---
+++ AutoSitzplatz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AutoSitzplatz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Auto bmw = new Auto(5);
+            for (int i = 0; i < 8; i++)
+            {
+                bmw.einsteigen(2);
+            }
+        }
+    }
+}
